264e6adeb212aa09219deaa997bcbb0003c50966
Lior Becker <liorbecker@gmail.com>
2013-02-24 01:21:43 +0200

    UI improvments
    Add logic to buttons

--- Tuple.UI.Split/ItemsPage.xaml.cs
+++ Tuple.UI.Split/ItemsPage.xaml.cs
@@ -18,6 +18,8 @@ using Windows.UI.Xaml.Navigation;
 using Tuple.Infra.Log;
 using Tuple.Logic.Mock;
 using Tuple.Logic.Interfaces;
+using System.Threading.Tasks;
+using Windows.UI;
 
 // The Items Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234233
 
@@ -30,8 +32,12 @@ namespace Tuple.UI.Split
     public sealed partial class ItemsPage : Tuple.UI.Split.Common.LayoutAwarePage
     {
 
-        private IGame game; 
-        private List<ICardWithPosition>  cardsWithPosition = new List<ICardWithPosition>();
+        private IGame game;
+        private List<Button> presedButtonsWithPosition = new List<Button>();
+        private Dictionary<uint, Button> orderButtinDic = new Dictionary<uint, Button>();
+        private SolidColorBrush brushYellowGreen = new SolidColorBrush(Colors.YellowGreen);
+        private Brush brushOriginal;
+        private readonly int delaymilisec = 400;
 
         public ItemsPage()
         {
@@ -51,31 +57,25 @@ namespace Tuple.UI.Split
         /// session.  This will be null the first time a page is visited.</param>
         protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
         {
-            // TODO: Create an appropriate data model for your problem domain to replace the sample data
-            //var sampleDataGroups = SampleDataSource.GetGroups((String)navigationParameter);
-            //this.DefaultViewModel["Items"] = sampleDataGroups;
 
-            
+            orderButtinDic[0] = Button0;
+            orderButtinDic[1] = Button1;
+            orderButtinDic[2] = Button2;
+            orderButtinDic[3] = Button3;
+            orderButtinDic[4] = Button4;
+            orderButtinDic[5] = Button5;
+            orderButtinDic[6] = Button6;
+            orderButtinDic[7] = Button7;
+            orderButtinDic[8] = Button8;
+            orderButtinDic[9] = Button9;
+            orderButtinDic[10] = Button10;
+            orderButtinDic[11] = Button11;
+            orderButtinDic[12] = Button12;
+            orderButtinDic[13] = Button13;
+            orderButtinDic[14] = Button14; 
 
-            //this.ite
-            //this.itemGridView.Items.
+            brushOriginal = Button1.BorderBrush;
 
-            int counter = 0;
-            while( game.ShouldOpenCard())
-            {
-                var card = game.OpenCard();
-                if(card != null)
-                {
-                    cardsWithPosition.Add(card);
-                    var b = (Button)this.itemGridView.Items[counter];
-                    counter++;
-                    b.Content = card.ToString();
-                    
-
-                    //TODO: do logic
-                }
-
-            }
 
         }
 
@@ -87,46 +87,119 @@ namespace Tuple.UI.Split
         /// <param name="e">Event data that describes the item clicked.</param>
         void ItemView_ItemClick(object sender, ItemClickEventArgs e)
         {
-            // Navigate to the appropriate destination page, configuring the new page
-            // by passing required information as a navigation parameter
-            //var groupId = ((SampleDataGroup)e.ClickedItem).UniqueId;
-            //this.Frame.Navigate(typeof(SplitPage), groupId);
+        }
 
-            var button = e.ClickedItem as Button;
-            //item.Title = "Click ";
-            //item.Subtitle = "Click ";
-            button.ClickMode = ClickMode.Press;
+        private async void Grid_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            
+           
 
-            //button.BorderBrush = new SolidColorBrush(new Windows.UI.Color);
-            //if(button
-
-            if(game.RemoveSet(cardsWithPosition[0], cardsWithPosition[1],cardsWithPosition[2]))
+            while (game.ShouldOpenCard() ) 
             {
-                //cardsWithPosition.Remove
-                while( game.ShouldOpenCard())
-                {
-                    var card = game.OpenCard();
-                    if(card != null)
-                    {
-                        cardsWithPosition.Add(card);
+              
+  
+                await Task.Delay(delaymilisec);
+                var card = game.OpenCard();
+                var position = (uint)card.Position.Row*3 + (uint)card.Position.Col;
 
-                        //button.Title = card.ToString();
-                        //button.Subtitle = card.Position.ToString();
-                        //TODO: do logic
-                    }
+                orderButtinDic[position].Visibility = Windows.UI.Xaml.Visibility.Visible;
+                orderButtinDic[position].Content = card;
 
-                }
             }
-            
-            game.IsGameOver();
-            
-
-            MetroEventSource.Log.Debug("Item clicked: " + button);
-
 
         }
 
 
+        private void ButtonN_Click(object sender, RoutedEventArgs e)
+        {
+            lock (game)
+            {
+
+                var button = (Button)e.OriginalSource;
+
+                //Flip color BorderBrush 
+                if (button.BorderBrush == brushYellowGreen)
+                {
+                    button.BorderBrush = brushOriginal;
+                    presedButtonsWithPosition.Remove(button);
+                }
+                else
+                {
+                    if (presedButtonsWithPosition.Count == 3)
+                        return;
+
+                    button.BorderBrush = brushYellowGreen;
+                    presedButtonsWithPosition.Add(button);
+                    
+                }
+
+                //Check for 3sum
+                if (presedButtonsWithPosition.Count == 3)
+                {
+                    if (game.RemoveSet(presedButtonsWithPosition[0].Content as ICardWithPosition,
+                        presedButtonsWithPosition[1].Content as ICardWithPosition,
+                        presedButtonsWithPosition[2].Content as ICardWithPosition))
+                    {
+
+                        //Hide the 3 cards
+                        presedButtonsWithPosition[0].Visibility = Visibility.Collapsed;
+                        presedButtonsWithPosition[1].Visibility = Visibility.Collapsed;
+                        presedButtonsWithPosition[2].Visibility = Visibility.Collapsed;
+
+                        //Check if game is over
+                        if (game.IsGameOver())
+                        {
+                            this.pageTitle.Text = "Game finished!";
+                            //TODO: game finished
+                        }
+
+                        ////////////////////////////
+                        //Open new cards
+                        //match  - remove 3 cards
+                        if (game.ShouldOpenCard())
+                        {
+                            //await Task.Delay(delaymilisec);
+                            presedButtonsWithPosition[0].Content = game.OpenCard();
+                            presedButtonsWithPosition[0].Visibility = Visibility.Visible;
+                        }
+                        if (game.ShouldOpenCard())
+                        {
+                            //await Task.Delay(delaymilisec);
+                            presedButtonsWithPosition[1].Content = game.OpenCard();
+                            presedButtonsWithPosition[1].Visibility = Visibility.Visible;
+                        }
+
+                        if (game.ShouldOpenCard())
+                        {
+                            //await Task.Delay(delaymilisec);
+                            presedButtonsWithPosition[2].Content = game.OpenCard();
+                            presedButtonsWithPosition[2].Visibility = Visibility.Visible;
+                        }
+                    }
+                }
+            }
+        }
+
+        #region Timer
+        public void StartTimer(object o, RoutedEventArgs sender)
+        {
+
+
+            DispatcherTimer myDispatcherTimer = new DispatcherTimer();
+            myDispatcherTimer.Interval = new TimeSpan(0, 0, 0, 1, 0); // 100 Milliseconds 
+            myDispatcherTimer.Tick += new EventHandler<object>(Each_Tick);
+            myDispatcherTimer.Start();
+        }
+
+        // A variable to count with.
+        int tickinsec = 0;
+
+        // Raised every 100 miliseconds while the DispatcherTimer is active.
+        public void Each_Tick(object o, object sender)
+        {
+            myTextBlock.Text = "Seconds: " + tickinsec++.ToString();
+        }
+        #endregion 
 
     }
 }
